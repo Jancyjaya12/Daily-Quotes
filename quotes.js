207f11cfbde2877ce8015560ef0903e062981290
Jancy Daniel <jancyjaya12@gmail.com>
2023-10-26 10:29:46 +0530

    added a file to read quotes.json

--- quotes.js
+++ quotes.js
@@ -1,6 +1,15 @@
-// A list of quotes.
-const quotes = [
-  "Put some quotes here..."
-]
+const fs = require('fs');
+
+let quoteData;
+try {
+  // Read the contents of 'quotes.json' file.
+  quoteData = fs.readFileSync('quotes.json');
+  console.log("✅ Successfully read 'quotes.json'")
+} catch {
+  console.error("⚠️  Could not read 'quotes.json'")
+}
+
+// Parse it from JSON string to JS Object
+const quotes = JSON.parse(quoteData ?? '[]');
 
 module.exports = quotes;
